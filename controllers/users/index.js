b5e62a7928d4a901d63cc40969b8aee04339ec98
IOksa <117656767+IOksa@users.noreply.github.com>
2023-09-27 16:40:30 +0300

    prepare project

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -5,8 +5,7 @@ const getCurrent=require('./getCurrent');
 const logout=require('./logout');
 const updateSubscription=require('./updateSubscription');
 const updateAvatar=require('./updateAvatar');
-const verifyEmail=require('./verifyEmail');
-const resendVerifyEmail= require('./resendVerifyEmail');
+
 
 module.exports = {
     register: ctrlWrapper(register),
@@ -15,6 +14,5 @@ module.exports = {
     logout: ctrlWrapper(logout),
     updateSubscription: ctrlWrapper(updateSubscription),
     updateAvatar: ctrlWrapper(updateAvatar),
-    verifyEmail: ctrlWrapper(verifyEmail),
-    resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
+
 }
