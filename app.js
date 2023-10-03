7c30b8c5d53f245c143b6a26e347b932a452e93a
IOksa <117656767+IOksa@users.noreply.github.com>
2023-10-03 08:25:18 +0300

    update swagger

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
@@ -17,7 +19,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
