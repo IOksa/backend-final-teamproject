ea2a91af50214aacc4b1d50cb0a397418b7a2861
IOksa <117656767+IOksa@users.noreply.github.com>
2023-10-11 20:18:28 +0300

    update

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -15,7 +15,7 @@ const register = async(req, res)=> {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-    const avatarURL = gravatar.url(email);
+   // const avatarURL = gravatar.url(email);
     const verificationToken = crypto.randomUUID();
 
     const newUser = await User.create({...req.body, password: hashPassword, avatarURL, verificationToken});
